bb5c6bf3068f558f433d94c607eb46e11f121855
vodchella <twister.kz@gmail.com>
2020-02-28 15:41:49 +0600

    Real forecast

--- medivh.py
+++ medivh.py
@@ -11,6 +11,14 @@ IDX_COL = 'date_idx'
 engine = create_engine('mysql+mysqlconnector://root:root@localhost/medivh')
 
 
+def get_daily_sales_by_barcode(barcode: int) -> DataFrame:
+    data = pd.read_sql(f'select date as date_idx, '
+                       f'       quantity '
+                       f'from   medivh.sales__by_day '
+                       f'where  barcode = {barcode}', con=engine)
+    return create_df_indexed_by_date(data)
+
+
 def create_df_indexed_by_date(data_frame: DataFrame, index_col: str = IDX_COL) -> DataFrame:
     index = pd.DatetimeIndex(pd.to_datetime(data_frame[index_col]))
     result = data_frame.set_index(index).sort_index()
@@ -54,28 +62,23 @@ def smooth_df(data_frame: DataFrame, beg: Arrow, end: Arrow):
     return create_df_indexed_by_date(DataFrame(arr, columns=[IDX_COL, 'quantity']))
 
 
-def combine_series(s1: Series, s2: Series):
-    def add_percent(v, percent):
-        return v * (1 + (percent / 100))
-
-    def is_nan_or_zero(v):
-        return np.isnan(v) or v == 0.0
-
-    def mean_with_weight(v1, v2):
-        if is_nan_or_zero(v1):
-            return add_percent(v2, -10)
-        else:
-            return add_percent(np.mean([v1, v2]), 10)
-
-    return s1.combine(s2, mean_with_weight)
+def compare_df(new_df: DataFrame, old_df: DataFrame, beg: Arrow, end: Arrow) -> float:
+    arr = []
+    for day in Arrow.range('day', beg, end):
+        new_val = new_df.loc[day.date()].values[0]
+        old_val = old_df.loc[day.date()].values[0]
+        percent_diff = 100 - (old_val * 100 / new_val)
+        arr.append(percent_diff)
+    return float(np.mean(arr))
 
 
-def get_daily_sales_by_barcode(barcode: int) -> DataFrame:
-    data = pd.read_sql(f'select date as date_idx, '
-                       f'       quantity '
-                       f'from   medivh.sales__by_day '
-                       f'where  barcode = {barcode}', con=engine)
-    return create_df_indexed_by_date(data)
+def increase_df(data_frame: DataFrame, inc_percent: float, beg: Arrow, end: Arrow):
+    arr = []
+    for day in Arrow.range('day', beg, end):
+        val = data_frame.loc[day.date()].values[0]
+        new_val = val + (val * inc_percent / 100)
+        arr.append([day.date(), new_val])
+    return create_df_indexed_by_date(DataFrame(arr, columns=[IDX_COL, 'quantity']))
 
 
 # 8887290101004 - coffee
@@ -83,19 +86,26 @@ def get_daily_sales_by_barcode(barcode: int) -> DataFrame:
 # 48742245      - parliament
 # 48743587      - winston
 
-now = arrow.get(2020, 2, 26)
-beg_date = arrow.get(2019, 12, 1)
-end_date = arrow.get(2020, 4, 30)
+last_data_date = arrow.get(2020, 2, 26)
+for_date = last_data_date.shift(months=1)
+
+beg_date = last_data_date.shift(months=-1)
+end_date = for_date
 df = get_daily_sales_by_barcode(48743587)
 
 real = create_df_with_zeroes(df, beg_date.shift(weeks=-1), end_date)
 old = create_df_with_zeroes(df, beg_date.shift(weeks=-1), end_date, lambda a: a.shift(years=-1))
-real_smoothed = smooth_df(real, beg_date, now)
+real_smoothed = smooth_df(real, beg_date, last_data_date)
 old_smoothed = smooth_df(old, beg_date, end_date)
 
-real.columns = ['real']
+percent = compare_df(real_smoothed, old_smoothed, beg_date, last_data_date)
+forecast = increase_df(old_smoothed, percent, last_data_date.shift(days=1), end_date)
+
 real.insert(len(real.columns), 'smoothed', real_smoothed)
 real.insert(len(real.columns), 'old', old_smoothed)
+real.insert(len(real.columns), 'forecast', forecast)
+
+real.columns = ['this_year_sales', 'this_year_sales_smoothed', 'past_year_sales_smoothed', 'forecast']
 
 real.plot()
 plt.show()
