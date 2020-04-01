24675c320139448f2bbd3b61a4ba7872ff540fec
vodchella <twister.kz@gmail.com>
2020-04-01 15:34:58 +0600

    Handle exceptions when can't get forecast

--- medivh.py
+++ medivh.py
@@ -26,9 +26,9 @@ products = {
 
 
 barcode = 4680036912629
-store_id = 110
-forecast_from_date = arrow.get(2020, 1, 25)
-forecast_before_date = forecast_from_date.shift(months=1)
+store_id = 1
+forecast_from_date = arrow.get(2019, 5, 25)
+forecast_before_date = forecast_from_date.shift(days=5)
 
 engine = create_engine('mysql+mysqlconnector://root:root@localhost/medivh')
 df_barcode = get_barcode_daily_sales(engine, store_id, barcode)
@@ -39,17 +39,23 @@ real_sales = smooth_df(create_df_with_zeroes(df_barcode, forecast_from_date, for
                        forecast_before_date)
 sales_series = real_sales['quantity'][forecast_from_date.date():forecast_before_date.date()]
 
-barcode_forecast = get_barcode_forecast(df_barcode, forecast_from_date, forecast_before_date)
-if barcode_forecast is not None:
-    print('First forecast:')
-    analyze_forecast(sales_series, barcode_forecast)
-    real_sales.insert(len(real_sales.columns), 'forecast_1', barcode_forecast)
+try:
+    barcode_forecast = get_barcode_forecast(df_barcode, forecast_from_date, forecast_before_date)
+    if barcode_forecast is not None:
+        print('First forecast:')
+        analyze_forecast(sales_series, barcode_forecast)
+        real_sales.insert(len(real_sales.columns), 'forecast_1', barcode_forecast)
+except:
+    print('Can\'t get first forecast')
 
-category_forecast = get_category_forecast(df_barcode, df_category, forecast_from_date, forecast_before_date)
-if category_forecast is not None:
-    print('Second forecast:')
-    analyze_forecast(sales_series, category_forecast)
-    real_sales.insert(len(real_sales.columns), 'forecast_2', category_forecast)
+try:
+    category_forecast = get_category_forecast(df_barcode, df_category, forecast_from_date, forecast_before_date)
+    if category_forecast is not None:
+        print('Second forecast:')
+        analyze_forecast(sales_series, category_forecast)
+        real_sales.insert(len(real_sales.columns), 'forecast_2', category_forecast)
+except:
+    print('Can\'t get second forecast')
 
 real_sales.plot(title=products[barcode])
 plt.show()
