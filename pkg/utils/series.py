ee8dd30b3c8aa9845e6daf05294dc890415f5775
vodchella <twister.kz@gmail.com>
2020-04-01 14:24:18 +0600

    Calculate accuracy errors instead of simple accuracy

--- pkg/utils/series.py
+++ pkg/utils/series.py
@@ -5,9 +5,9 @@ from pandas import Series
 def percent_accuracy(fval: float, sval: float) -> float:
     if fval != 0.0:
         diff = abs(fval - sval)
-        perc = 100.0 - min(diff * 100 / fval, 100.0)
+        perc = diff * 100 / fval
     else:
-        perc = 0.0
+        perc = sval
     return perc
 
 
@@ -19,12 +19,8 @@ def is_series_correlated(first: Series, second: Series):
     return get_series_correlation(first, second) >= 75.0
 
 
-def get_series_accuracy(first: Series, second: Series):
+def get_series_accuracy_errors(first: Series, second: Series):
     arr = []
     for index, value in second.items():
         arr.append(percent_accuracy(first[index], value))
     return np.median(arr)
-
-
-def get_series_value_accuracy(first: Series, second: Series):
-    return percent_accuracy(first.sum(), second.sum())
