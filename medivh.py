e835625f4c0f033674db652df4732251db28f1c3
vodchella <twister.kz@gmail.com>
2020-04-02 15:08:35 +0600

    Load mysql connection params from yaml config

--- medivh.py
+++ medivh.py
@@ -3,10 +3,13 @@
 import argparse
 import arrow
 import matplotlib.pyplot as plt
+import yaml
 from pandas import Series
 from pkg.data import get_barcode_daily_sales, get_category_daily_sales
 from pkg.forecast import get_barcode_forecast, get_category_forecast, get_mean_forecast
+from pkg.utils.console import panic
 from pkg.utils.df import create_df_with_zeroes
+from pkg.utils.files import read_file
 from pkg.utils.series import get_forecast_accuracy_errors, get_forecast_standard_deviation
 from sqlalchemy import create_engine
 
@@ -27,6 +30,7 @@ def create_argparse():
     )
     return parser.parse_args()
 
+
 products = {
     8887290101004: 'Coffee 3 in 1',
     5449000133328: 'Coca cola',
@@ -43,40 +47,51 @@ forecast_from_date = arrow.get(2019, 5, 25)
 forecast_before_date = forecast_from_date.shift(days=5)
 
 args = create_argparse()
+if args.config:
+    config = yaml.safe_load(read_file(args.config))
+    print(f'Config loaded from {args.config}')
 
-engine = create_engine('mysql+mysqlconnector://root:root@localhost/medivh')
-df_barcode = get_barcode_daily_sales(engine, store_id, barcode)
-df_category = get_category_daily_sales(engine, store_id, barcode)
+    db = config['mysql']
+    db_path = '%s:%s@%s:%s/%s' % (db['user'], db['pass'], db['host'], db['port'], db['schema'])
+    db_secured_path = '%s:%s@%s:%s/%s' % (db['user'], '*****', db['host'], db['port'], db['schema'])
+    engine = create_engine(f'mysql+mysqlconnector://{db_path}')
+    print(f'Connected to MySQL at {db_secured_path}')
 
-real_sales = create_df_with_zeroes(df_barcode, forecast_from_date, forecast_before_date)
-sales_series = real_sales['quantity'][forecast_from_date.date():forecast_before_date.date()]
+    df_barcode = get_barcode_daily_sales(engine, store_id, barcode)
+    df_category = get_category_daily_sales(engine, store_id, barcode)
 
-try:
-    barcode_forecast = get_barcode_forecast(df_barcode, forecast_from_date, forecast_before_date)
-    if barcode_forecast is not None:
-        print('First forecast:')
-        analyze_forecast(sales_series, barcode_forecast)
-        real_sales.insert(len(real_sales.columns), 'forecast_1', barcode_forecast)
-except:
-    print('Can\'t get first forecast')
+    real_sales = create_df_with_zeroes(df_barcode, forecast_from_date, forecast_before_date)
+    sales_series = real_sales['quantity'][forecast_from_date.date():forecast_before_date.date()]
 
-try:
-    category_forecast = get_category_forecast(df_barcode, df_category, forecast_from_date, forecast_before_date)
-    if category_forecast is not None:
-        print('Second forecast:')
-        analyze_forecast(sales_series, category_forecast)
-        real_sales.insert(len(real_sales.columns), 'forecast_2', category_forecast)
-except:
-    print('Can\'t get second forecast')
+    try:
+        barcode_forecast = get_barcode_forecast(df_barcode, forecast_from_date, forecast_before_date)
+        if barcode_forecast is not None:
+            print('First forecast:')
+            analyze_forecast(sales_series, barcode_forecast)
+            real_sales.insert(len(real_sales.columns), 'forecast_1', barcode_forecast)
+    except:
+        print('Can\'t get first forecast')
 
-try:
-    mean_forecast = get_mean_forecast(df_barcode, forecast_from_date, forecast_before_date)
-    if mean_forecast is not None:
-        print('Third forecast:')
-        analyze_forecast(sales_series, mean_forecast)
-        real_sales.insert(len(real_sales.columns), 'forecast_3', mean_forecast)
-except:
-    print('Can\'t get third forecast')
+    try:
+        category_forecast = get_category_forecast(df_barcode, df_category, forecast_from_date, forecast_before_date)
+        if category_forecast is not None:
+            print('Second forecast:')
+            analyze_forecast(sales_series, category_forecast)
+            real_sales.insert(len(real_sales.columns), 'forecast_2', category_forecast)
+    except:
+        print('Can\'t get second forecast')
 
-real_sales.plot(title=products[barcode])
-plt.show()
+    try:
+        mean_forecast = get_mean_forecast(df_barcode, forecast_from_date, forecast_before_date)
+        if mean_forecast is not None:
+            print('Third forecast:')
+            analyze_forecast(sales_series, mean_forecast)
+            real_sales.insert(len(real_sales.columns), 'forecast_3', mean_forecast)
+    except:
+        print('Can\'t get third forecast')
+
+    real_sales.plot(title=products[barcode])
+    plt.show()
+
+else:
+    panic('No config file specified. Use -c option')
